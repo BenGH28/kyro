03b5611016471c28d7647d3ea73a25bc6c8b9246
Ben Hunt <bigben.hunt@gmail.com>
2021-09-08 22:43:41 -0600

    use anyhow::result in main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use kyro::*;
+mod cli;
+use cli::Command;
+use structopt::StructOpt;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> anyhow::Result<()> {
+    //read the config
     let config = Config::get_config()?;
     if let Some(lang_code) = config.get_language_code() {
-        download_bible(&lang_code, &config)?
+        download_bible(&lang_code, &config)?;
+
+        let args = Command::from_args();
+        args.run()
+        //the config is good so we now have to work with the cli
     }
     Ok(())
 }
